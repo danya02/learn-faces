394ba197c9e117aa0434e65b141d82772426aa33
Danya Generalov <danya@danya02.ru>
2021-09-01 01:32:14 +0300

    Tweaking build targets and refactoring to AndroidX. Now available on Play.

--- app/src/main/java/ru/danya02/learnfaces/DatabaseViewActivity.java
+++ app/src/main/java/ru/danya02/learnfaces/DatabaseViewActivity.java
@@ -1,9 +1,9 @@
 package ru.danya02.learnfaces;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.widget.Toast;
 
 import org.json.JSONException;
--- app/src/main/java/ru/danya02/learnfaces/UpdaterResultActivity.java
+++ app/src/main/java/ru/danya02/learnfaces/UpdaterResultActivity.java
@@ -1,9 +1,9 @@
 package ru.danya02.learnfaces;
 
 import android.content.Intent;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
